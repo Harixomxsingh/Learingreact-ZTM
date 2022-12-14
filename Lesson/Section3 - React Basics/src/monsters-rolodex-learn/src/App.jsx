6bff9abd280f8ddf456a7782674265f27855c66f
Hari om Singh <www.hariom03.com@gmail.com>
2022-12-14 20:54:03 +0530

    Day 3 wrap up

--- Lesson/Section3 - React Basics/src/monsters-rolodex-learn/src/App.jsx
+++ Lesson/Section3 - React Basics/src/monsters-rolodex-learn/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState, Component } from "react";
-import reactLogo from "./assets/react.svg";
+import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
   // const [count, setCount] = useState(0)
+  constructor() {
+    super();
+    this.state = {
+      name: "Sumit",
+    };
+  }
 
   render() {
     return (
       <div className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <div>Hi, {this.state.name}</div>
+        <button
+          onClick={() => {
+            this.setState({ name: "Hari om Singh" });
+          }}
+        >
+          Change name
+        </button>
       </div>
     );
   }
