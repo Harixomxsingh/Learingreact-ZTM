9a95b97aa97b1041211b36671c7359bbc43255d5
Hari om Singh <www.hariom03.com@gmail.com>
2022-12-15 19:12:40 +0530

    render element using array method called map

--- Lesson/Section3 - React Basics/src/monsters-rolodex-learn/src/App.jsx
+++ Lesson/Section3 - React Basics/src/monsters-rolodex-learn/src/App.jsx
@@ -3,24 +3,46 @@ import "./App.css";
 
 class App extends Component {
   // const [count, setCount] = useState(0)
+  // let's render the element using map
   constructor() {
     super();
     this.state = {
-      name: "Sumit",
+      monsters: [
+        {
+          name: "Linda",
+        },
+        {
+          name: "Frank",
+        },
+        {
+          name: "Jacky",
+        },
+        {
+          name: "Sparky",
+        },
+        {
+          name: "Tomy",
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <div>Hi, {this.state.name}</div>
-        <button
+        {/* <div>Hi, {this.state.name}</div> */}
+        <div>
+          {this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>;
+          })}
+        </div>
+        {/* <button
           onClick={() => {
             this.setState({ name: "Hari om Singh" });
           }}
         >
           Change name
-        </button>
+        </button> */}
       </div>
     );
   }
