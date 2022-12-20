92fe503a94816fc339455b640a95b2c409589713
Hari om Singh <www.hariom03.com@gmail.com>
2022-12-20 15:39:22 +0530

    added css to the whole project

--- Lesson/Section3 - React Basics/src/monsters-rolodex-learn/src/components/card-list/card-list.component.jsx
+++ Lesson/Section3 - React Basics/src/monsters-rolodex-learn/src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import "./css/cardList.style.css";
 class cardList extends Component {
   render() {
     let monsters = this.props.monsters;
@@ -6,10 +7,16 @@ class cardList extends Component {
     console.log(monsters);
     return (
       <div>
-        <section>
+        <h1>Monsters Park</h1>
+        <section className="box">
           {monsters.map((monster) => {
             return (
-              <div key={monster.id}>
+              <div key={monster.id} className="card-container">
+                <img
+                  src={`https://robohash.org/${monster.id}?set=set2`}
+                  alt={`monster : ${monster.name}`}
+                  className="monster__img"
+                />
                 <h1>Name: {monster.name}</h1>
                 <p>Username: {monster.username}</p>
                 <i>email: {monster.email}</i>
