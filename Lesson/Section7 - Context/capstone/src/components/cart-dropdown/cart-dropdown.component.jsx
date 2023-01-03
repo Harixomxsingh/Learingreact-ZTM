5e6bcd68e87e3d21507a9ad85deef6a36ef41d91
Hari om Singh <www.hariom03.com@gmail.com>
2023-01-03 13:44:11 +0530

    fix all bug

--- Lesson/Section7 - Context/capstone/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ Lesson/Section7 - Context/capstone/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Button } from "../button/button.component";
-
+import "./cart-dropdown.styles.scss";
 const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div
+        className="cart-items"
+        style={{
+          // height: "400px",
+          // backgroundColor: "red",
+          zIndex: "10",
+        }}
+      ></div>
       <Button buttonType={"inverted"} />
     </div>
   );
